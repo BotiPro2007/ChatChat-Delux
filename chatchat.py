5310bc01363608896dc46417269272022491c7a7
BotiPro2007 <boti@evonit.hu>
2022-07-21 16:08:25 +0200

    Hitboxes and full map

--- chatchat.py
+++ chatchat.py
@@ -1,16 +1,28 @@
 import pygame
-import pyscreeze
 
 pygame.init()
 
 map_img = pygame.image.load("map.png")
-#pyscreeze.locateAll()
+hitmap_img = pygame.image.load("hitmap.png")
+"""
+walls = [[(21, 28), 0], [(0, 28), 1], [(0, 28), 2], [(0, 28), 3]]
+
+Image.frombuffer("RGBX", (16,16),map_img.subsurface(pygame.Rect((1825, 1), (16, 16))).get_buffer().raw).show()
+
+for row in walls:
+    for cube in range(row[0][0], row[0][1]):
+        cube = cube*16
+        map_img.subsurface(pygame.Rect((1602+cube, 2+row[1]*16), (16, 16)))
+        #pyscreeze.locateAll()
+"""
+
 scale = 2
 cat = pygame.image.load("greencat.png")
 icon = pygame.image.load("logo.png")
 #icon = pygame.transform.scale(cat.subsurface(pygame.Rect((16*4, 0+2), (8, 8))), (35, 35))
-map_chunks = {"house": pygame.Rect((320*2, 192*2), (320, 192))}
-playspace = map_img.subsurface(map_chunks["house"]).copy() #(156, 96)
+playspace = map_img.subsurface(pygame.Rect((320*2, 192*2), (320, 192))).copy() #(156, 96)
+
+hitmap_array = pygame.surfarray.array2d(hitmap_img.subsurface(pygame.Rect((320*2, 192*2), (320, 192))))
 #playspace.fill((0,0,0))
 d = pygame.display.set_mode((playspace.get_width()*scale, playspace.get_height()*scale), pygame.RESIZABLE)
 pygame.display.set_caption("ChatChat // DELUX //")
@@ -52,12 +64,13 @@ class Character(pygame.sprite.Sprite):
         self.right = pygame.K_RIGHT
 
         self.rect = self.image.get_rect()
-        
+
+        self.chunk_y = 2
+        self.chunk_x = 2
+        self.hitmap_array = pygame.surfarray.array2d(hitmap_img.subsurface(pygame.Rect((320*self.chunk_x, 192*self.chunk_y), (320, 192))))
         self.x = 160
-        self.prevX = 0
         self.nameX = self.rect.centerx - (len(self.name) * 5)
         self.y = 128
-        self.prevY = 0
 
     def onUpdate(self):
         self.move()
@@ -69,33 +82,53 @@ class Character(pygame.sprite.Sprite):
     def move(self):
         if tick % 3 == 0:
             keys = pygame.key.get_pressed()
-            if not self.rect.collidepoint((self.rect.topleft[0], 0)):
-                if keys[self.up]:
+            if keys[self.up]:
+                if not self.rect.collidepoint((self.rect.topleft[0], 0)) and not (self.hitmap_array[self.rect.centerx][self.rect.centery - 16] == 65280):
                     self.y -= 16
                     if self.image_index == 1: self.image_index = 0
                     elif self.image_index == 0: self.image_index = 1
                     else: self.image_index = 0
                     return
-            if not self.rect.collidepoint((self.rect.bottomleft[0], playspace.get_height()-16)):
-                if keys[self.down]:
+                elif self.rect.collidepoint((self.rect.topleft[0], 0)):
+                    self.chunk_y -= 1
+                    self.hitmap_array = pygame.surfarray.array2d(hitmap_img.subsurface(pygame.Rect((320*self.chunk_x, 192*self.chunk_y), (320, 192))))
+                    self.y = playspace.get_height()-16
+                    return
+            if keys[self.down]:
+                if not self.rect.collidepoint((self.rect.bottomleft[0], playspace.get_height()-16)) and not (self.hitmap_array[self.rect.centerx][self.rect.centery + 16] == 65280):
                     self.y += 16
                     if self.image_index == 2: self.image_index = 3
                     elif self.image_index == 3: self.image_index = 2
                     else: self.image_index = 2
                     return
-            if not self.rect.collidepoint((start_x, self.rect.topleft[1])):
-                if keys[self.left]:
+                elif self.rect.collidepoint((self.rect.bottomleft[0], playspace.get_height()-16)):
+                    self.chunk_y += 1
+                    self.hitmap_array = pygame.surfarray.array2d(hitmap_img.subsurface(pygame.Rect((320*self.chunk_x, 192*self.chunk_y), (320, 192))))
+                    self.y = 0
+                    return
+            if keys[self.left]:
+                if not self.rect.collidepoint((start_x, self.rect.topleft[1])) and not (self.hitmap_array[self.rect.centerx - 16][self.rect.centery] == 65280):
                     self.x -= 16
                     if self.image_index == 4: self.image_index = 5
                     elif self.image_index == 5: self.image_index = 4
                     else: self.image_index = 4
                     return
-            if not self.rect.collidepoint((start_x+playspace.get_width()-4, self.rect.topright[1])):
-                if keys[self.right]:
+                elif self.rect.collidepoint((start_x, self.rect.topleft[1])):
+                    self.chunk_x -= 1
+                    self.hitmap_array = pygame.surfarray.array2d(hitmap_img.subsurface(pygame.Rect((320*self.chunk_x, 192*self.chunk_y), (320, 192))))
+                    self.x = start_x+playspace.get_width()-16
+                    return
+            if keys[self.right]:
+                if not self.rect.collidepoint((start_x+playspace.get_width()-4, self.rect.topright[1])) and not (self.hitmap_array[self.rect.centerx + 16][self.rect.centery] == 65280):
                     self.x += 16
                     if self.image_index == 6: self.image_index = 7
                     elif self.image_index == 7: self.image_index = 6
                     else: self.image_index = 6
+                    return
+                elif self.rect.collidepoint((start_x+playspace.get_width()-4, self.rect.topright[1])):
+                    self.chunk_x += 1
+                    self.hitmap_array = pygame.surfarray.array2d(hitmap_img.subsurface(pygame.Rect((320*self.chunk_x, 192*self.chunk_y), (320, 192))))
+                    self.x = start_x
                     return
             if self.image_index % 2 > 0: self.image_index -= 1
 
@@ -104,12 +137,11 @@ class Character(pygame.sprite.Sprite):
         self.rect = playspace.blit(self.image, (self.x + start_x, self.y))
 
     def show_name(self):
-        posx = min(max(me.rect.centerx-80, 8), 152)
-        posy = min(max(me.rect.centery-48, 8), 88)
+        posx = min(max(self.rect.centerx-80, 8), 152)
+        posy = min(max(self.rect.centery-48, 8), 88)
         fr = pygame.Rect((posx, posy), (160, 96))
-        print(fr.centerx)
-        self.nameX = me.rect.centerx-fr.topleft[0] - (len(self.name) * 1.2)
-        self.nameY = me.rect.centery-fr.topleft[1]+10
+        self.nameX = self.rect.centerx-fr.topleft[0] - (len(self.name) * 1.2)
+        self.nameY = self.rect.centery-fr.topleft[1]+10
         resized_playspace.blit(globalFont.render(self.name, True, (255,255,255)), (self.nameX*4, (self.nameY*4)))
     
 class Dog(Character):
@@ -192,13 +224,12 @@ class Input(Component):
 
 me = Cat("Csacsi")
 #notme = Dog("Someone else")
-#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
 
 chat_input = Input("Itt lesz majd a chat", (playspace.get_width()*scale, playspace.get_height()*scale), (1000,50), (255,255,255))
 
 while playing:
     #playspace.fill((0,0,0))
-    playspace = map_img.subsurface(map_chunks["house"]).copy()
+    playspace = map_img.subsurface(pygame.Rect((320*me.chunk_x, 192*me.chunk_y), (320, 192))).copy()
     for event in pygame.event.get(eventtype=pygame.QUIT): playing = False
     me.onUpdate()
     posx = min(max(me.rect.centerx-80, 8), 152)
